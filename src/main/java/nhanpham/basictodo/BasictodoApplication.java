65f835a60a1ac4c3955ad344a5c3a21a89f4b5c7
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2023-05-20 10:30:55 +0700

    first commit

---
+++ src/main/java/nhanpham/basictodo/BasictodoApplication.java
@@ -0,0 +1,13 @@
+package nhanpham.basictodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasictodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasictodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhanpham/basictodo/BasictodoApplicationTests.java
@@ -0,0 +1,13 @@
+package nhanpham.basictodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasictodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
